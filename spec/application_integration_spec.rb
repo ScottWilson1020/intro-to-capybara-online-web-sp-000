dbc9de330ff0b48a8d968adcffc45747a0cabd79
Scott Wilson <scottwilson1020@gmail.com>
2020-12-13 16:06:21 +0000

    Automatically backed up by Learn

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -4,7 +4,7 @@ describe "GET '/' - Greeting Form" do
   # Code from previous example
   it 'welcomes the user' do
     visit '/'
-    expect(page.body).to include("Sup Bitch!")
+    expect(page.body).to include("Welcome!")
   end
 
   # New test
